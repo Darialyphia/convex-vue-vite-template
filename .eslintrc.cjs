1ca8413f7a9dca30d44ec8b816b5262cb6a895f3
loicpennequin <lo.pennequin@gmail.com>
2023-08-30 22:36:22 +0200

    setup eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-undef': 'off',
+    'no-redeclare': 'off',
+    'no-unused-vars': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-setup-props-destructure': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  }
+};
