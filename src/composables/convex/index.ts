f47798239b51198e810dbe947450f315a7b828d9
loicpennequin <lo.pennequin@gmail.com>
2023-09-02 20:57:26 +0200

    added barrel file in composables/convex

---
+++ src/composables/convex/index.ts
@@ -0,0 +1,7 @@
+export { useConvex, useConvexAuth } from './useConvex';
+export { useQuery } from './useQuery';
+export { useSuspenseQuery } from './useSuspenseQuery';
+export { useQueries } from './useQueries';
+export { useMutation } from './useMutation';
+export { useAction } from './useAction';
+export { usePaginatedQuery } from './usePaginatedQuery';
